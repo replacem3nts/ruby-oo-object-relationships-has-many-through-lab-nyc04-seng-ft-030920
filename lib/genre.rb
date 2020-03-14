c979deddaff93648dbcc52054644b54ba7eba837
replacem3nts <donny.landis@gmail.com>
2020-03-13 21:20:48 -0400

    updates files to pass all tests

---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+
+require "pry"
+class Genre
+attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self}
+    end
+
+    def artists
+        songs.map {|song| song.artist}
+    end
+
+end
